1f4336d3bb2ce9e8d9f8d6dcf4204ccc2e8fa1f6
jaimesilvalorca <jaimesilvalorca@gmail.com>
2022-12-28 02:14:56 -0300

    desafio up!

--- assets/js/Pokedex.js
+++ assets/js/Pokedex.js
@@ -1,90 +1,33 @@
-// alert('Bienvenido al pokedexjs.... En construcción')
-
-// let listaPokemon = []
-
-// // los objetos los voy a alimentar desde una api luego
-
-// class Pokemon{
-//     constructor(numero,nombre,tipo,generacion){
-//         this.numero = numero
-//         this.nombre = nombre
-//         this.tipo = tipo
-//         this.generacion = generacion
-//     }
-// }
-
-// // estos pokemon se van a desplegar de manera automatica en una especie de card en el html
-
-// const pokemon1 = new Pokemon(1,'Bulbasaur','Planta','Primera Generacion')
-// const pokemon2 = new Pokemon(2,'Pikachu','Electrico','Primera Generacion')
-// const pokemon3 = new Pokemon(3,'Charizard','Fuego','Primera Generacion')
-// const pokemon4 = new Pokemon(4,'Blastoise','Agua','Primera Generacion')
-// const pokemon5 = new Pokemon(5,'Alakazam','Psiquico','Primera Generacion')
-// const pokemon6 = new Pokemon(6,'Gengar','Fantasma','Primera Generacion')
-
-// listaPokemon = [pokemon1,pokemon2,pokemon3,pokemon4,pokemon5,pokemon6]
-
-// alert('Podras agregar tus propios pokemon al pokedex para dotarlo')
-
-// let pregunta = prompt('Quieres agregar un nuevo pokemon?')
-
-// const agregarNuevoPokemon = () => {
-
-//     let numero = prompt("Ingresa el numero del pokemon que vas agregar")*1
-//     let nombre = prompt("Ingresa el nombre del pokemon que vas a agregar")
-//     let tipo = prompt("Ingresa el tipo del pokemon que vas agregar")
-//     let generacion = prompt("Ingresa la generacion del pokemon que vas agregar")
-
-//     const pokemon7 = new Pokemon(numero,nombre,tipo,generacion)
-
-//     listaPokemon.push(pokemon7)
-
-//     return listaPokemon
-
-// }
-
-// //va a tener un buscador arriba de las cards
-
-// const buscarPokemon = () =>{
-//     consulta = prompt(`Ingresa el numero del pokemon que quieres consultar recuerda que el maximo es: ${listaPokemon.length}`)*1
-//     consulta = consulta -1
-
-//     for(i=0; i<listaPokemon.length; i++){
-//         if((consulta) === listaPokemon[i]){
-//         }
-//     }
-//     return console.log(listaPokemon[consulta])
-// }
-
-// const contarPokemon = () =>{
-//     contar = listaPokemon.length
-//     return contar
-// }
-
-// if((pregunta == 'si') || (pregunta == 'Si') || (pregunta == 'SI')){
-//     contador = contarPokemon()
-//     alert(`Actualmente tiene ${contador} pokemones`)
-//     agregarNuevoPokemon()
-//     console.log(listaPokemon)
-// }
-// else if((pregunta == 'no') || (pregunta == 'No') || (pregunta == 'NO')){
-//     buscarPokemon()
-//     console.log(listaPokemon)
-// }
-// else{
-//     alert("Se va a cerrar la pestaña... Adios")
-//     window.close()
-// }
-
 const listaPokemon = document.querySelector("#listaPokemon");
+const pokemon = document.getElementById('namePokemon')
+const buttonSearch = document.getElementById('searchPokemon')
 
 let URL = "https://pokeapi.co/api/v2/pokemon/"
+
+buttonSearch.addEventListener('onclick',getPokemon())
 
 for(let i = 1; i <= 151; i++){
     fetch(URL+i)
     .then((response) => response.json())
     .then(data => mostrarPokemon(data))
 }
+
+class Pokemon{
+  constructor(numero,nombre,tipo){
+      this.numero = numero
+      this.nombre = nombre
+      this.tipo = tipo
+
+  }
+}
+const pokemon1 = new Pokemon(0,'No encontrado','Sin tipo')
+
+const pokemon1String = JSON.stringify(pokemon1);
+localStorage.setItem('pokemon1', pokemon1String);
+
+// el objeto guardado en el localstorage se va a utilizar cuando el buscador no encuentre ningun pokemon, se va a mostrar ese objeto, en construccion......
+
+
 
 function mostrarPokemon (data) {
   const div = document.createElement("div");
@@ -95,13 +38,13 @@ function mostrarPokemon (data) {
     <div class="card-body">
       <ul>
         <li>
-            Numero:${data.id} 
+            Numero: ${data.id} 
         </li>
         <li>
-            Nombre:${data.name}
+            Nombre: ${data.name}
         </li>
         <li>
-            Tipo:${data.types.type}
+            Tipo: ${data.types[0].type.name}
         </li>
       </ul>
     </div>
@@ -109,3 +52,15 @@ function mostrarPokemon (data) {
 </div>`;
   listaPokemon.append(div);
 };
+
+async function getPokemon() {
+  let pokemon = document.getElementById("namePokemon").value;
+  let data =  await fetch(`${URL}${pokemon}`);                
+  data = await data.json();
+  listaPokemon.innerHTML="";
+  mostrarPokemon(data);
+}
+
+
+document.getElementById("searchPokemon").addEventListener("click", getPokemon);
+
